3edbb45066edf8e76a61121b758fd38e48b44285
Graziano Blasilli <33848564+blasilli@users.noreply.github.com>
2022-12-09 14:52:49 +0100

    created SurVis browser

---
+++ survis/data/tag_categories.js
@@ -0,0 +1,40 @@
+define({
+	tagCategories: {
+		"xdl_category": {
+			"description": ""
+		},
+		"application_domain": {
+			"description": ""
+		},
+		"model": {
+			"description": ""
+		},
+		"users": {
+			"description": ""
+		},
+		"interactivity": {
+			"description": ""
+		},
+		"phase": {
+			"description": ""
+		},
+		"source_code": {
+			"description": ""
+		},
+		"evaluation": {
+			"description": ""
+		},
+		"visualizations": {
+			"description": ""
+		},
+		"analytics_family": {
+			"description": ""
+		},
+		"analytics_subject": {
+			"description": ""
+		},
+		"analytics_novelty": {
+			"description": ""
+		},
+	}
+});
---
+++ survis/properties.js
@@ -0,0 +1,84 @@
+/**
+ * Name as used as title for the main page
+ * @type {string}
+ */
+var title = 'The State of The Art of Visual Analytics for eXplainable Deep Learning';
+
+/**
+ * Relative path to the data directory
+ * @type {string}
+ */
+var dataDir = 'data/';
+
+/**
+ * Relative path to the js directory
+ * @type {string}
+ */
+var jsDir = 'js/';
+
+/**
+ * Relative path to the styles directory
+ * @type {string}
+ */
+var stylesDir = 'styles/';
+
+/**
+ * Options for displaying tag clouds.
+ * field: BibTeX field used for generating the tag clouds,
+ * title: Title displayed as headline
+ * minTagFrequency: minimum frequency of a tag to be displayed
+ * @type {{field: string, title: string, minTagFrequency: number}[]}
+ */
+var tagCloudOptions = [{
+    field: 'keywords',
+    title: 'Keywords',
+    minTagFrequency: 1
+}, 
+{
+    field: 'venue',
+    title: 'Venue',
+    minTagFrequency: 1
+},{
+    field: 'author',
+    title: 'Authors',
+    minTagFrequency: 1
+}];
+
+/**
+ * If BibTeX entries (and tags) should be editable
+ * @type {boolean}
+ */
+var editable = false;
+
+/**
+ * Subtitle describing the paper the data is referring to (set to null to deactivate)
+ * @type {{html: string, id: string}}
+ */
+var paper = {
+    html: 'Biagio La Rosa, Graziano Blasilli, Romain Bourqui, David Auber, Giuseppe Santucci, Roberto Capobianco, Enrico Bertini, Romain Giot, and Marco Angelini.<br>Literature collection. <b><a href="https://doi.org/xxx/yyy" target="_blank">https://doi.org/xxx/yyy</a></b>',
+};
+
+/**
+ * Extra pages like an about page that will be listed in the footer an can be opened on demand (page name -> path)
+ */
+var extraPages = {
+    'about': 'about.html'
+};
+
+/**
+ * Custom style as path to an extra css file (leave empty if not applicable)
+ * @type {string}
+ */
+var customStyle = '';
+/* Example for simplified entries */
+//var customStyle = 'styles/custom_entries_simplified.css';
+
+/**
+ * Options for showing citation information (set to null to avoid showing any citation information)
+ * minCitationCount: minimum number of citations of a paper to be displayed in the visualization
+ * @type {{minCitationCount: number}}
+ */
+/* var citations = {
+    minCitationCount: 5
+}; */
+var citations = null;
